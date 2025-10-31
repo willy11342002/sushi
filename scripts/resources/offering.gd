8fa0f574409c02590331b8530b078a6c6465a880
0336_william <0336_willilam@rfr.com.tw>
2025-10-31 17:32:20 +0800

    feature: 優化offering公式

--- scripts/resources/offering.gd
+++ scripts/resources/offering.gd
@@ -8,29 +8,77 @@ class_name Offering extends Resource
 func _init(_sushi_type: SushiType) -> void:
 	sushi_type = _sushi_type
 
-
+#region Appearance
 func appearance() -> float:
+	return base_appearance() * appearance_multiple()
+
+func appearance_multiple() -> float:
+	return sushi_type.appearance_multiple
+
+func base_appearance() -> float:
 	var _appearance: float = 0.0
 	for ingredient in ingredients:
 		_appearance += ingredient.appearance
 	return _appearance
+#endregion
 
+
+#region Aroma
 func aroma() -> float:
+	return base_aroma() * aroma_multiple()
+
+func aroma_multiple() -> float:
+	return sushi_type.aroma_multiple
+
+func base_aroma() -> float:
 	var _aroma: float = 0.0
 	for ingredient in ingredients:
 		_aroma += ingredient.aroma
 	return _aroma
+#endregion
 
+#region Taste
 func taste() -> float:
+	return base_taste() * taste_multiple()
+
+func taste_multiple() -> float:
+	return sushi_type.taste_multiple
+
+func base_taste() -> float:
 	var _taste: float = 0.0
 	for ingredient in ingredients:
 		_taste += ingredient.taste
 	return _taste
+#endregion
 
-
+#region Price
 func price() -> float:
-	return 0.0
+	return base_price() * price_multiple()
 
+func price_multiple() -> float:
+	return sushi_type.price_multiple
 
+func base_price() -> float:
+	var _price: float = 0.0
+	for ingredient in ingredients:
+		_price += (
+			ingredient.appearance * sushi_type.appearance_weight +
+			ingredient.aroma * sushi_type.aroma_weight +
+			ingredient.taste * sushi_type.taste_weight
+		)
+	return _price
+#endregion
+
+#region Complexity
 func complexity() -> float:
-	return 0.0
+	return base_complexity() * complexity_multiple()
+
+func complexity_multiple() -> float:
+	return sushi_type.complexity_multiple
+
+func base_complexity() -> float:
+	var _complexity: float = 0.0
+	for ingredient in ingredients:
+		_complexity += ingredient.complexity
+	return _complexity
+#endregion
--- scripts/resources/sushi_type.gd
+++ scripts/resources/sushi_type.gd
@@ -9,17 +9,18 @@ class_name SushiType extends Resource
 @export var total_ingredient_count: int = 1
 
 @export_group("Quality")
-@export var appearance: float = 1.0
-@export var aroma: float = 1.0
-@export var taste: float = 1.0
+@export var appearance_multiple: float = 1.0
+@export var aroma_multiple: float = 1.0
+@export var taste_multiple: float = 1.0
 
 @export_group("Price")
+@export var price_multiple: float = 1.0
 @export var appearance_weight: float = 1.0
 @export var aroma_weight: float = 1.0
 @export var taste_weight: float = 1.0
 
 @export_group("Complexity")
-@export var complexity: float = 1.0
+@export var complexity_multiple: float = 1.0
 
 var description: String:
 	get:
