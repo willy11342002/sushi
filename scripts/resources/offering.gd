fdcb7527911d81ad20c067381ec6e8061ccf5b13
0336_william <0336_willilam@rfr.com.tw>
2025-10-31 17:17:54 +0800

    debug: 菜色五數結構優化

--- scripts/resources/offering.gd
+++ scripts/resources/offering.gd
@@ -1,23 +1,36 @@
 class_name Offering extends Resource
 
+
 @export var sushi_type: SushiType
 @export var ingredients: Array[Ingredient] = []
 
-var appearance: float:
-	get:
-		return ingredients.map(func (i): return i.appearance).reduce(func (a, b): return a + b, 0.0)
-var aroma: float:
-	get:
-		return ingredients.map(func (i): return i.aroma).reduce(func (a, b): return a + b, 0.0)
-var taste: float:
-	get:
-		return ingredients.map(func (i): return i.taste).reduce(func (a, b): return a + b, 0.0)
-var price: float:
-	get:
-		return 0.0
-var complexity: float:
-	get:
-		return 0.0
 
 func _init(_sushi_type: SushiType) -> void:
 	sushi_type = _sushi_type
+
+
+func appearance() -> float:
+	var _appearance: float = 0.0
+	for ingredient in ingredients:
+		_appearance += ingredient.appearance
+	return _appearance
+
+func aroma() -> float:
+	var _aroma: float = 0.0
+	for ingredient in ingredients:
+		_aroma += ingredient.aroma
+	return _aroma
+
+func taste() -> float:
+	var _taste: float = 0.0
+	for ingredient in ingredients:
+		_taste += ingredient.taste
+	return _taste
+
+
+func price() -> float:
+	return 0.0
+
+
+func complexity() -> float:
+	return 0.0
